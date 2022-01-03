2fd58801a1186a031ad83105a435576237437c00
zhangyujie <zhangyujie02@dobest.com>
2022-01-03 18:52:01 +0800

    tools用法

---
+++ src/main/java/com/example/study/concurrent/tools/CountDownLatchTest.java
@@ -0,0 +1,56 @@
+package com.example.study.concurrent.tools;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * <h3>study</h3>
+ * <p>CountDownLatch用法</p>
+ *
+ * @author : ZhangYuJie
+ * @date : 2022-01-03 17:49
+ **/
+
+public class CountDownLatchTest {
+    public static void main(String[] args) {
+        final CountDownLatch latch = new CountDownLatch(3);
+        new Thread(()->{
+            try {
+                System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+                Thread.sleep(3000);
+                System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
+                latch.countDown();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(()->{
+            try {
+                System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+                Thread.sleep(3000);
+                System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
+                latch.countDown();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(()->{
+            try {
+                System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+                Thread.sleep(3000);
+                System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
+                latch.countDown();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        try {
+            System.out.println("等待3个子线程执行完毕...");
+            latch.await();
+            System.out.println("3个子线程已经执行完毕");
+            System.out.println("继续执行主线程");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
